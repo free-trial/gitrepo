317ca5d6714f15e0ee30a4cbdd603eb85d3f808b
rahulaaaa <rahulaaaa@mailinator.com>
2013-12-29 16:42:19 +0530

    commit by vishnu

--- GitRepo/FirstGitProject/src/RunGitApp.java
+++ GitRepo/FirstGitProject/src/RunGitApp.java
@@ -7,6 +7,8 @@ public class RunGitApp {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Git!!!");
+		System.out.println("commit using vishnu's login. --1");
+		System.out.println("commit using vishnu's login.");
 	}
 
 }
