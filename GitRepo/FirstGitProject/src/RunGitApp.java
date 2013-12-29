12e9ac4b29279565a1686133d81a31f7e251ea1d
rahulaaaa <rahulaaaa@mailinator.com>
2013-12-29 17:05:48 +0530

    try

---
+++ GitRepo/FirstGitProject/src/RunGitApp.java
@@ -0,0 +1,13 @@
+
+public class RunGitApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello Git!!!");
+		System.out.println("commit using vishnu's login.");
+	}
+
+}
